406c769b5f86c54c8a2c79b4b5c4c2c10f0d2ccc
Adarsh Agrawal <54720756+adarshagrawal38@users.noreply.github.com>
2025-11-28 13:07:07 +0530

    Fix CVES: Bumping up version of tools and rag-ui (#153)
    By rebuilding `Tools` and the `Rag-ui` image, the latest version of the
    `expat` package will be installed, helping to resolve the high-severity
    CVEs.
    
    Signed-off-by: Adarsh Agrawal <adarsh.agrawal1@ibm.com>

--- ai-services/internal/pkg/vars/var.go
+++ ai-services/internal/pkg/vars/var.go
@@ -5,7 +5,7 @@ import "regexp"
 var (
 	// SpyreCardAnnotationRegex -> ai-services.io/<containerName>--spyre-cards
 	SpyreCardAnnotationRegex = regexp.MustCompile(`^ai-services\.io\/([A-Za-z0-9][-A-Za-z0-9_.]*)--spyre-cards$`)
-	ToolImage                = "icr.io/ai-services-cicd/tools:0.3"
+	ToolImage                = "icr.io/ai-services-cicd/tools:0.4"
 	ModelDirectory           = "/var/lib/ai-services/models"
 )
 
