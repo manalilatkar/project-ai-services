4cc662163e6b0266e7a0af5e3a05d50d545c7e4e
Mayuka Channankaiah <Mayuka.C@ibm.com>
2026-01-12 11:54:38 +0530

    [deps] Remove react-router-dom (#258)
    - There is a CVE with react router dom:
    https://github.com/IBM/project-ai-services/security/dependabot/12
    - We do not even require the react-router-dom pkg at all as we are not
    providing any multipage routing functionalities, so have removed the
    library itself as it is not required
    - Also remove cors, env-cmd and web-vitals as these pkgs are not being
    used.
    
    ---------
    
    Signed-off-by: mayuka-c <Mayuka.C@ibm.com>

--- spyre-rag/ui/src/index.jsx
+++ spyre-rag/ui/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import { GlobalTheme } from '@carbon/react';
 import App from './components/App.jsx';
@@ -9,9 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalTheme theme={'g100'}>
-      <BrowserRouter forceRefresh={false}>
-        <App />
-      </BrowserRouter>
+      <App />
     </GlobalTheme>
   </React.StrictMode>,
 );
